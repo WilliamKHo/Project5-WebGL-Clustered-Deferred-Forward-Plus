1f6323401e3bd33f7b93e76c1ec18bc04a71809a
WilliamKHo <willho@seas.upenn.edu>
2017-11-02 23:05:07 -0400

    weakest lights

--- src/scene.js
+++ src/scene.js
@@ -23,9 +23,9 @@ class Scene {
           Math.random() * (LIGHT_MAX[2] - LIGHT_MIN[2]) + LIGHT_MIN[2],
         ]),
         color: new Float32Array([
-          0.1 + 0.4 * Math.random(),
-          0.1 + 0.4 * Math.random(),
-          0.1 + 0.6 * Math.random(),
+          0.0 + 0.2 * Math.random(),
+          0.0 + 0.2 * Math.random(),
+          0.0 + 0.3 * Math.random(),
         ]),
         radius: LIGHT_RADIUS,
       });
